0f08c554d147fd1c0847368d6db77bc083d7dba6
Larry Karani Kubende <larry@Larrys-MacBook-Pro.local>
2019-03-13 20:28:31 +0300

    Add secret key

--- bookstore/bookstore/settings.py
+++ bookstore/bookstore/settings.py
@@ -18,6 +18,7 @@ env = environ.Env(
     DEBUG = (bool, False)
 )
 environ.Env.read_env()
+SECRET_KEY = config('SECRET_KEY')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
