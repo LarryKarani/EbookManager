7b46e207df066d8d85a60387d52dc5847290fb69
Larry Karani Kubende <larry@Larrys-MacBook-Pro.local>
2019-03-21 22:04:01 +0300

    [Finishes #164682979] User signup.

--- bookstore/books/urls.py
+++ bookstore/books/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import CreateView
-from .views import DetailsView 
+from .views import DetailsView
+from .views import CreateUserView 
 
 
 urlpatterns = {
     path('auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/v1/books/', CreateView.as_view(), name="create"),
-    path('api/v1/books/<int:pk>/', DetailsView.as_view(), name="details")
+    path('api/v1/books/<int:pk>/', DetailsView.as_view(), name="details"),
+    path('api/v1/auth/signup/', CreateUserView.as_view(), name='register')
 }
+
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- bookstore/books/views.py
+++ bookstore/books/views.py
@@ -1,12 +1,11 @@
 from rest_framework import generics
-from .serializers import BookSerializer
+from .serializers import BookSerializer, UserSerializer
 from .models import Book
 from rest_framework import permissions
 class CreateView(generics.ListCreateAPIView):
     """This class defines the add book behavior of the rest API"""
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = (permissions.IsAuthenticated,)
     def perform_create(self, serializer):
         """ Save the post when adding a new book """
         serializer.save(created_by=self.request.user)
@@ -15,3 +14,7 @@ class DetailsView(generics.RetrieveUpdateDestroyAPIView):
     """This class handles the http GET, PUT and DELETE requests."""
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+class CreateUserView(generics.CreateAPIView):
+    """Create user view"""
+    serializer_class = UserSerializer
